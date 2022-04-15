ac5df1bcd7e1ad5c27fcddfa337e80febfda27c2
Guadalupe Diaz <diazmariaguadalupe946@gmail.com>
2022-04-15 00:18:43 -0300

    funciones parte 2

--- script.js
+++ script.js
@@ -194,7 +194,7 @@ const ventasDelMes = (mes, anio) => {
       venta.fecha.getMonth() === mes - 1 && venta.fecha.getFullYear() === anio
   );
 };
-//console.log(ventasDelMes(1, 2019));
+console.log(ventasDelMes(2, 2019));
 
 const contadorVentas = (ventas) => {
   let acc = 0;
@@ -266,4 +266,21 @@ const ventasSucursal = (sucursal) => {
   }
   return acc;
 };
-//console.log(ventasSucursal(""));
+//console.log(ventasSucursal("Centro"));
+
+//9-Crear la función sucursalDelMes(mes, anio), que se le pasa dos parámetros numéricos, (mes, anio) y devuelve el nombre de la sucursal que más vendió en plata en el mes. No cantidad de ventas, sino importe total de las ventas. El importe de una venta es el que indica la función precioMaquina. El mes es un número entero que va desde el 1 (enero) hasta el 12 (diciembre).
+const sucursalDelMes = (mes, anio) => {
+  let sucursalQueMasVendio = "";
+  let acc = 0;
+  for (const sucursal of sucursales) {
+    if (
+      acc <
+      contadorVentas(
+        ventasDelMes(mes, anio).filter((venta) => venta.sucursal === sucursal)
+      )
+    ) {
+    }
+  }
+  return sucursalQueMasVendio;
+};
+console.log(sucursalDelMes(1, 2019));
